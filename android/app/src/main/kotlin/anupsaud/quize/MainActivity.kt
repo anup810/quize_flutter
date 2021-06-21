caff0f456069c0865d2b0c576f2e90c69f0b2b91
Anup Saud <anupsaud10@gmail.com>
2021-06-21 07:55:55 +0545

    Initial Commit

---
+++ android/app/src/main/kotlin/anupsaud/quize/MainActivity.kt
@@ -0,0 +1,6 @@
+package anupsaud.quize
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quize());
+
+class Quize extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+class QuizePage extends StatefulWidget {
+  @override
+  _QuizePageState createState() => _QuizePageState();
+}
+
+class _QuizePageState extends State<QuizePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
