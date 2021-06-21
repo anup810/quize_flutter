5e367ffdbf397bd5f802e34e1f82858142482065
Anup Saud <anupsaud10@gmail.com>
2021-06-21 09:39:27 +0545

    Added Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quize());
@@ -5,7 +6,14 @@ void main() => runApp(Quize());
 class Quize extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: QuizePage(),
+        ),
+      ),
+    );
   }
 }
 
@@ -17,6 +25,66 @@ class QuizePage extends StatefulWidget {
 class _QuizePageState extends State<QuizePage> {
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where question will go.',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                //True Pressed by User
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {
+                //False Pressed by user.
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
